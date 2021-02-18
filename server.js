36b0a9b3b3f807652f59f71e21efa37ed2d139d2
Nkemjika Obi <nkemjikaobi@Administrators-MacBook-Pro.local>
2021-02-18 16:30:38 +0100

    Set up package.json for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db')
 const morgan = require('morgan')
+const path = require('path');
 
 const app = express();
 
@@ -20,6 +21,14 @@ if(app.get('env') === 'development'){
 //Define Routes
 app.use('/api/logs', require('./routes/logs'));
 app.use('/api/techs', require('./routes/techs'));
+
+ //Serve static assets in production
+ if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client' , 'build' , 'index.html')));
+ }
 
 
 
